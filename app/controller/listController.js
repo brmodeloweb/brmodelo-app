bcec171bc1e5f8f5de975d49c8050ca555dd1c2f
miltonbsn <miltonbst@gmail.com>
2016-02-15 22:58:29 -0200

    Main table

--- app/controller/listController.js
+++ app/controller/listController.js
@@ -1,7 +1,9 @@
 angular.module('myapp').controller('listController', function($scope, ModelAPI){
-	$scope.models = [{'name': 'h1'}, {'name': 'h3'}, {'name': 'h2'}];
 
 	ModelAPI.getAllModels().then(function(models){
+
+		console.log(models.data);
+
 		$scope.models = models.data;
 	});
 });
