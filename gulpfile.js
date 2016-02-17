9be719706a63fcdd188bb74758a05f54a307534d
idmarjr <idmarjr@gmail.com>
2016-02-17 00:26:15 -0200

    Tabs are (guess what?) TABS! Not doble spaces. :|

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-// gulpfile.js
 var gulp = require('gulp');
 var server = require('gulp-express');
 
 gulp.task('copy', function() {
-    gulp.src(['node_modules/angular-ui-bootstrap/ui-bootstrap.js',
-    'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.min.js',
-    'node_modules/angular-modal-service/dst/angular-modal-service.min.js',
-    'node_modules/angular/angular.min.js',
-    'node_modules/angular-ui-router/build/angular-ui-router.min.js',
-    'node_modules/angular-cookies/angular-cookies.min.js'
-  ]).pipe(gulp.dest('build/libs/'));
+	gulp.src([
+		'node_modules/angular-ui-bootstrap/ui-bootstrap.js',
+		'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.min.js',
+		'node_modules/angular-modal-service/dst/angular-modal-service.min.js',
+		'node_modules/angular/angular.min.js',
+		'node_modules/angular-ui-router/build/angular-ui-router.min.js',
+		'node_modules/angular-cookies/angular-cookies.min.js'
+	]).pipe(gulp.dest('build/libs/'));
 
-    gulp.src(['node_modules/bootstrap/dist/**/*'
-  ]).pipe(gulp.dest('build/css/bootstrap'));
+	gulp.src([
+		'node_modules/bootstrap/dist/**/*'
+	]).pipe(gulp.dest('build/css/bootstrap'));
 
-    gulp.src(['app/css/Sweet-Feedback/css/sweetfeedback.css',
-    'app/css/Sweet-Feedback/images/iconError.png',
-    'app/css/Sweet-Feedback/images/iconInfo.png',
-    'app/css/Sweet-Feedback/images/iconSuccess.png',
-    'app/css/Sweet-Feedback/images/iconWarning.png',
-    'app/css/Sweet-Feedback/images/noise.png',
-    'app/css/Sweet-Feedback/images/noiseStripes.png',
-  ]).pipe(gulp.dest('build/sweetfeedback/'));
-
-});
+	gulp.src([
+		'app/css/Sweet-Feedback/css/sweetfeedback.css',
+		'app/css/Sweet-Feedback/images/iconError.png',
+		'app/css/Sweet-Feedback/images/iconInfo.png',
+		'app/css/Sweet-Feedback/images/iconSuccess.png',
+		'app/css/Sweet-Feedback/images/iconWarning.png',
+		'app/css/Sweet-Feedback/images/noise.png',
+		'app/css/Sweet-Feedback/images/noiseStripes.png',
+	]).pipe(gulp.dest('build/sweetfeedback/'));
+});//End task copy
 
 gulp.task('server', function () {
-    // Start the server at the beginning of the task
-    server.run(['server.js']);
-});
+	server.run(['server.js']);
+}); //End task server
 
 gulp.task('default', ['copy','server']);
