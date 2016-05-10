b4b5deda5f27b8f704e8f92d504b18b09d2e613c
miltonbsn <miltonbst@gmail.com>
2016-05-10 00:25:20 -0300

    custom modal

--- app/angular/controller/modal/newModelModalController.js
+++ app/angular/controller/modal/newModelModalController.js
@@ -4,11 +4,14 @@ app.controller('ModelModalController', function($scope, $uibModalInstance, $root
 
 	$scope.selected = 'conceptual';
 
-	$scope.save = function(newmodel) {
-		newmodel.user = $rootScope.loggeduser;
-		newmodel.type = $scope.selected;
-		newmodel.model = '{"cells":[]}';
-		$uibModalInstance.close(newmodel);
+	$scope.save = function(validForm, newmodel) {
+		console.log(validForm);
+		if(validForm){
+			newmodel.user = $rootScope.loggeduser;
+			newmodel.type = $scope.selected;
+			newmodel.model = '{"cells":[]}';
+			$uibModalInstance.close(newmodel);
+		}
 	};
 
 	$scope.cancel = function() {
--- app/angular/controller/modellist/listController.js
+++ app/angular/controller/modellist/listController.js
@@ -2,20 +2,20 @@ var app = angular.module('myapp');
 
 app.controller('listController', function($scope, $state, ModelAPI, $rootScope, $uibModal) {
 
-	var vm = this;
-	vm.models = [];
+	var self = this;
+	self.models = [];
 
 	ModelAPI.getAllModels($rootScope.loggeduser).then(function(models) {
-		vm.models = models.data;
+		self.models = models.data;
 	});
 
-	vm.openModel = function(model) {
+	self.openModel = function(model) {
 		$state.go(model.type, {
 			'modelid': model._id
 		});
 	};
 
-	vm.newModel = function() {
+	self.newModel = function() {
 		var modalInstance = $uibModal.open({
 			animation: true,
 			templateUrl: 'angular/view/modal/newModelModal.html',
@@ -24,15 +24,15 @@ app.controller('listController', function($scope, $state, ModelAPI, $rootScope, 
 
 		modalInstance.result.then(function (model) {
 			ModelAPI.saveModel(model).then(function(newModel){
-				vm.openModel(newModel);
+				self.openModel(newModel);
 			});
 		});
 	};
 
-	vm.deleteModel = function(model) {
+	self.deleteModel = function(model) {
 		ModelAPI.deleteModel(model._id).then(function (resp) {
 			if (resp.status === 200){
-				vm.models.splice(vm.models.indexOf(model), 1);
+				self.models.splice(self.models.indexOf(model), 1);
 			}
 		});
 	};
