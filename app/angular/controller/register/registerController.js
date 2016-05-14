f1dfe160853148ec2775d9bce5910b8a67d78b54
miltonbsn <miltonbst@gmail.com>
2016-05-14 16:37:56 -0300

    Register code and css refac

--- app/angular/controller/register/registerController.js
+++ app/angular/controller/register/registerController.js
@@ -1,70 +1,40 @@
-var app = angular.module('myapp');
-
-app.controller('registerController', function($scope, $state, AuthService) {
-
-	$scope.credentials = {
+var registerController = function($state, AuthService) {
+	var self = this;
+	self.submitted = false;
+	self.feedback = {
+		message: "",
+		showing: false
+	}
+	self.credentials = {
 		username: '',
 		email: '',
 		password: ''
 	};
 
-	$scope.config = {
-		submitted: false,
-		validmail: false,
-		validname: false,
-		validpassword: false,
-		useralreadyexist: false
-	}
+	self.submitForm = function(validForm) {
+		self.submitted = true;
+		self.feedback.showing = false;
+		if (validForm) {
+			doRegister();
+		} else {
+			showError("Preencha os campos em vermelho");
+		}
+	};
 
-	$scope.register = function(credentials, repassword) {
-		$scope.config.submitted = true;
-
-		isValidName(credentials.username);
-		isValidPassword(credentials.password, repassword)
-		isValidEmail(credentials.email)
-
-		if ($scope.config.validmail && $scope.config.validname && $scope.config.validpassword) {
-			AuthService.register(credentials).then(function(user) {
-				$state.go('login');
+	function doRegister(){
+		AuthService.register(self.credentials).then(
+			function(user) {
+				$state.go('main');
 			}, function(error) {
-				$scope.config.useralreadyexist = true;
-			});
-		}
-	};
-
-	$scope.cancel = function() {
-		$modalInstance.dismiss('cancel');
-	};
-
-	function isValidName(field) {
-		if (field == null || field == "") {
-			$scope.config.validname = false;
-			return false;
-		} else {
-			$scope.config.validname = true;
-			return true;
-		}
+				showError("Email já cadastrado");
+		});
 	}
 
-	function isValidPassword(pass1, pass2) {
-		if ((pass1 == pass2 && pass1.length > 3)) {
-			$scope.config.validpassword = true;
-			return true;
-		} else {
-			$scope.config.validpassword = false;
-			return false;
-		}
+	function showError(newMessage){
+		self.feedback.message = newMessage;
+		self.feedback.showing = true;
 	}
 
-	function isValidEmail(field) {
-		var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if (!re.test(field)) {
-			$scope.config.validmail = false;
-			return false;
-		} else {
-			$scope.config.validmail = true;
-			return true;
-		}
-	}
+};
 
-});
+angular.module('myapp').controller('registerController', registerController);
