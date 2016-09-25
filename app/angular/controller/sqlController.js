8b65ee4109eef207d9817846c318efe44d0c7eb2
Milton Bittencourt <miltonbst@gmail.com>
2016-09-25 11:11:21 -0300

    Limpando código

--- app/angular/controller/sqlController.js
+++ app/angular/controller/sqlController.js
@@ -1,9 +1,6 @@
 var sqlController = function(AuthService, $state, $scope) {
 
 	var self = this;
-
-	$scope.pagex = "meu peru";
-
 };
 
 angular.module('myapp').controller('sqlController', sqlController);
