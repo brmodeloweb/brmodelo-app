d6039df86ff91e2b271bd9291f6104b448dfdd5a
Milton Bittencourt <miltonbst@gmail.com>
2021-02-14 13:55:47 -0300

    Remove forgotten console.lo

--- app/joint/joint.ui.halo.js
+++ app/joint/joint.ui.halo.js
@@ -107,7 +107,7 @@ joint.ui.Halo = Backbone.View.extend({
         this.listenTo(this.options.graph, "all", this.update), 
         this.listenTo(this.options.paper, "blank:pointerdown halo:create", this.remove), 
         this.listenTo(this.options.paper, "scale translate", this.update), 
-        //this.listenTo(this.options.cellView.model, "remove", this.remove), 
+        this.listenTo(this.options.cellView.model, "remove", this.remove), 
         $(document.body).on("mousemove touchmove", this.pointermove), 
         $(document).on("mouseup touchend", this.pointerup), 
         this.options.paper.$el.append(this.$el),
