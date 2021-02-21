117bade513a9497b5775d9d15f96392b8e924fff
Milton Bittencourt <miltonbst@gmail.com>
2021-02-20 23:45:50 -0300

    login component cleanup

--- app/angular/login/login.js
+++ app/angular/login/login.js
@@ -1,9 +1,8 @@
 import angular from "angular";
-import newModelModalController from "../controller/modal/newModelModalController";
 import authService from "../service/authService";
 import template from "./login.html";
 
-const LoginController = function (AuthService, $state, $uibModal) {
+const LoginController = function (AuthService, $state) {
 	const ctrl = this;
 	ctrl.submitted = false;
 	ctrl.credentials = {};
@@ -11,24 +10,29 @@ const LoginController = function (AuthService, $state, $uibModal) {
 		message: "",
 		showing: false,
 	};
+
 	const showError = (newMessage) => {
 		ctrl.feedback.message = newMessage;
 		ctrl.feedback.showing = true;
 	};
+
 	const handleLoginError = () => {
 		ctrl.loading = false;
 		showError("Login ou senha incorretos");
 	};
+
 	const handleLoginSuccess = () => {
 		ctrl.loading = false;
 		$state.go("main");
 	};
+
 	const doLogin = () => {
 		ctrl.loading = true;
 		AuthService.login(ctrl.credentials)
 			.then(handleLoginSuccess)
 			.catch(handleLoginError);
 	};
+
 	ctrl.submitForm = (validForm) => {
 		ctrl.submitted = true;
 		ctrl.feedback.showing = false;
@@ -39,22 +43,9 @@ const LoginController = function (AuthService, $state, $uibModal) {
 		}
 	};
 
-	ctrl.openModal = () => {
-		var modalInstance = $uibModal.open({
-			animation: true,
-			templateUrl: 'angular/view/modal/newModelModal.html',
-			controller:  'ModelModalController'
-		});
-
-		modalInstance.result.then(function (model) {
-			ModelAPI.saveModel(model).then(function(newModel){
-				self.openModel(newModel);
-			});
-		});
-	}
 };
 
-export default angular.module("app.login", [authService, newModelModalController]).component("login", {
+export default angular.module("app.login", [authService]).component("login", {
 	template,
 	controller: LoginController,
 }).name;
