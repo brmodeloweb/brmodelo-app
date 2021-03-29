20ab0ce76444e823565e5f3c7c0720f8c8470ada
Milton Bittencourt <miltonbst@gmail.com>
2021-03-29 01:02:47 -0300

    Create erd factory module

---
+++ app/angular/conceptual/factory.js
@@ -0,0 +1,35 @@
+export default class Factory {
+
+  constructor(shapes) {
+    this.shapes = shapes;
+  }
+
+  createEntity = (customConfig) => {
+		return new this.shapes.erd.Entity(customConfig);
+	};
+
+	createRelationship = (customConfig) => {
+		return new this.shapes.erd.Relationship(customConfig);
+	};
+
+	createIsa = (customConfig) => {
+		return new this.shapes.erd.Isa(customConfig);
+	};
+
+	createAssociative = (customConfig) => {
+		return new this.shapes.erd.Associative(customConfig)
+	};
+
+	createAttribute = (customConfig) => {
+		return new this.shapes.erd.Attribute(customConfig);
+	};
+
+	createKey = (customConfig) => {
+		return new this.shapes.erd.Key(customConfig);
+	};
+
+  createLink = (customConfig) => {
+		return new this.shapes.erd.Link(customConfig);
+	};
+
+}
