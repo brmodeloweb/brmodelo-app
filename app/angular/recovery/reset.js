08b65dc35be8829306d59b1487941b3665717025
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2021-06-22 10:09:01 +0200

    Feature/password recovery (#165)
    * Login page recovery option
    
    - Add link to recovery password at login screen
    - .separator styles
    - .btn-alt styles
    
    * Reset password markup
    
    - Step 1 - Recovery: Enter email
    - Step 2 - Email (Not present in this commit)
    - Step 3 - Reset: Redefine password
    
    * Create base recovery endpoint
    
    * Create to link to recovery module
    
    * Create recovery component
    
    * Wireup recovery component
    
    * Finish recovery component
    
    * Create reset endpoint
    
    * Create reset component
    
    * Wireup reset component
    
    * Reduce top white space for public pages
    
    We notice some users using narrow screens (Or screens with lower resolution) are having issues of content
    being hidden under vertical scrool. To mitigate this problem, I'm reducing the gap between viewport
    top and content to all public pages.
    
    * Bubfix: Adjust .br-loader position
    
    * Create mailService
    
    * Create email template
    
    * Remove unused files
    
    Co-authored-by: miltonbst@gmail.com <miltonbst@gmail.com>

---
+++ app/angular/recovery/reset.js
@@ -0,0 +1,96 @@
+const ResetPasswordController = function (AuthService, $stateParams, $timeout, $state) {
+	const ctrl = this;
+	ctrl.submitted = false;
+	ctrl.mail = "";
+	ctrl.validation = {
+		validCode: false,
+	}
+	ctrl.feedback = {
+		message: "",
+		showing: false,
+		type: "error"
+	};
+
+	const setValidation = (valid) => {
+		$timeout(() => {
+			ctrl.validation.validCode = valid;
+		}, 100);
+	};
+
+	const setLoading = (loading) => {
+		$timeout(() => {
+			ctrl.loading = loading;
+		}, 100);
+	};
+
+	const showFeedback = (newMessage, type = "error") => {
+		$timeout(() => {
+			ctrl.feedback.message = newMessage;
+			ctrl.feedback.showing = true;
+			ctrl.feedback.type = type;
+		}, 100);
+	};
+
+	const handleError = (error) => {
+		console.log(error);
+		ctrl.loading = false;
+		showFeedback("Esse email ainda não está cadastrado.");
+	};
+
+	const handleSuccess = () => {
+		showFeedback(`Nova senha salva com sucesso.`, "success");
+		$timeout(() => {
+			setLoading(false);
+			$state.go("login");
+		}, 2000);
+	};
+
+	const doReset = (newPassword) => {
+		ctrl.loading = true;
+		const code = $stateParams.code;
+		const mail = $stateParams.mail;
+		AuthService.resetPassword(mail, code, newPassword)
+			.then(handleSuccess)
+			.catch(handleError);
+	};
+
+	ctrl.submitForm = (validForm, newPassword, repeatedPassword) => {
+		ctrl.submitted = true;
+		ctrl.feedback.showing = false;
+		if (validForm) {
+			if(newPassword != repeatedPassword) {
+				showFeedback("A senha deve ser a mesma nos dois campos");
+			} else if (newPassword.length < 6) {
+				showFeedback("Senha deve conter mais que 6 dígitos");
+			} else {
+				doReset(newPassword)
+			}
+		} else {
+			showFeedback("Preencha os campos em vermelho");
+		}
+	};
+
+	ctrl.$onInit = async () => {
+		setLoading(true);
+		const code = $stateParams.code;
+		const mail = $stateParams.mail;
+		try {
+			setLoading(true);
+			const response = await AuthService.validateRecovery(mail, code);
+			setLoading(false);
+			if (response.data.valid) {
+				setValidation(true);
+			} else {
+				showFeedback("Código de recuperação de senha é inválido, solicite a recuperação de senha novamente!");
+			}
+		} catch (error) {
+			showFeedback("Código de recuperação de senha é inválido, solicite a recuperação de senha novamente!");
+		}
+	}
+
+};
+
+angular.module("myapp").component("resetPassword", {
+	templateUrl: "angular/recovery/reset.html",
+	controller: ResetPasswordController,
+});
