90ed61354ba8ae3f12360c93a0dba2c0ad05e7be
Arthur Mota <arthurmotaa9@gmail.com>
2021-06-22 21:26:30 -0300

    Deploy webpack attempt 5

--- server_app/app.js
+++ server_app/app.js
@@ -9,21 +9,21 @@ const ejs = require("ejs");
 
 let app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	const webpack = require('webpack');
-	const config = require('../webpack.config.js');
-	const compiler = webpack(config);
+// if (process.env.NODE_ENV === 'production') {
+// 	const webpack = require('webpack');
+// 	const config = require('../webpack.config.js');
+// 	const compiler = webpack(config);
 	
-	const webpackDevMiddleware = require('webpack-dev-middleware');
-	//const webpackHotMiddleware = require('webpack-hot-middleware');
+// 	const webpackDevMiddleware = require('webpack-dev-middleware');
+// 	const webpackHotMiddleware = require('webpack-hot-middleware');
 	
-	app.use(webpackDevMiddleware(compiler, {
-			publicPath: config.output.publicPath,
-		})
-	)
+// 	app.use(webpackDevMiddleware(compiler, {
+// 			publicPath: config.output.publicPath,
+// 		})
+// 	)
 	
-	//app.use(webpackHotMiddleware(compiler))
-};
+// 	app.use(webpackHotMiddleware(compiler))
+// };
 
 app.engine("html", ejs.renderFile);
 
