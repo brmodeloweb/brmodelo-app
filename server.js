1f0bbba1d9f857f13cfd9d0c0fc115899f5a5bdc
Márcio Santos <marcio.tbms@gmail.com>
2021-06-28 22:01:11 -0300

    Configure dotenv in the server application

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.Promise = require("bluebird");
 const app = require("./server_app/app");
 
 const port = Number(process.env.PORT || 3000);
-
-const mongoport =
-	process.env.PROD_MONGODB || "mongodb://localhost:27017/brmodeloDB";
-// https://mlab.com/
+const apiUrl = process.env.API_URL || "http://localhost";
+const mongoUrl = process.env.PROD_MONGODB || process.env.MONGODB_URL;
+const isDevelopment = process.env.NODE_ENV === "development";
 
 mongoose.set("debug", true);
 mongoose.connect(
-	mongoport,
+	mongoUrl,
 	{ useNewUrlParser: true, useUnifiedTopology: true },
 	function (err) {
 		if (err) throw err;
+
+		const databaseInfo = isDevelopment ? `Database: ${mongoUrl}\n` : "";
+
 		app.listen(port, function () {
 			console.log(`
----------------------------------------------------
---------------- APPLICATION RUNNING ---------------
----------------------------------------------------
-App: http://localhost:${port}
-MongoDB: ${mongoport}
----------------------------------------------------
+			---------------------------------------------------
+			--------------- APPLICATION RUNNING ---------------
+			---------------------------------------------------
+
+			App: ${apiUrl}:${port}
+			${databaseInfo}
+			---------------------------------------------------
 		`);
 		});
 	}
