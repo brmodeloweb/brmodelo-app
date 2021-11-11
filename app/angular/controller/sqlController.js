63c79d7fc7f8ae0a0e7621a2113d607b6d9915ab
Milton Bittencourt <miltonbst@gmail.com>
2021-11-10 21:28:08 -0300

    Migration clean up (#236)
    * Logic sidebar empty state
    
    * Delete unused files

--- app/angular/controller/sqlController.js
+++
@@ -1,6 +0,0 @@
-var sqlController = function(AuthService, $state, $scope, $stateParams) {
-	var self = this;
-	$scope.sql = $stateParams.code;
-};
-
-angular.module('myapp').controller('sqlController', sqlController);
