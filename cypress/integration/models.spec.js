ec6a29927050c2101a764c7e646840edad6546de
Walmyr <wlsf82@gmail.com>
2021-11-18 01:36:05 +0100

    Increase test coverage of model creation
    This is a POC for testing drag-and-drop actions.

--- cypress/integration/models.spec.js
+++ cypress/integration/models.spec.js
@@ -31,12 +31,32 @@ describe('Models', () => {
     })
   
     it('creates a logical model', () => {
+      // Selects the logical model and save
       cy.get('.modelselect').click()
       cy.contains('li span', 'Lógico').click()
       cy.contains('button', 'Salvar').click()
-  
+      // Asserts the model was created
       cy.contains('h2', `Modelo lógico de: ${modelTitle}`)
         .should('be.visible')
+      // Adds two tables, connects them, and save
+      cy.dragAndDropTableAt(200, 200)
+      cy.dragAndDropTableAt(500, 200)
+      cy.get('.paper-scroller .joint-type-uml-class')
+        .first()
+        .click()
+      cy.get('.link')
+        .drag('.paper-scroller .joint-type-uml-class:nth-child(2)')
+      cy.contains('a', 'Salvar').click()
+      // Asserts the success message is displayed
+      cy.contains('.alert-success p', 'Salvo com sucesso!')
+        .should('be.visible')
+      // Reloads the page so that the success message disapears
+      cy.reload()
+      // Asserts the tables and connection are still there
+      cy.get('.paper-scroller .joint-type-uml-class')
+        .should('have.length', 2)
+      cy.get('.connection-wrap')
+        .should('have.length', 1)
     })
   })
 })
@@ -53,4 +73,9 @@ Cypress.Commands.add('cleanUpUserModelsAndReoload', () => {
     })
   })
   cy.reload()
+})
+
+Cypress.Commands.add('dragAndDropTableAt', (x, y) => {
+  cy.get('.joint-type-uml-class')
+    .move({ deltaX: x, deltaY: y })
 })
