f4be9bd59be31cb019e92203679e4ac07b2c07bd
Márcio <marcio.tbms@gmail.com>
2021-11-22 21:55:33 +0100

    fix: rever code format

--- app/angular/conceptual/conceptual.js
+++ app/angular/conceptual/conceptual.js
@@ -1,6 +1,8 @@
 import "backbone";
 import $ from "jquery";
+
 import * as joint from "jointjs/dist/joint";
+
 import "../../joint/joint.ui.stencil";
 import "../../joint/joint.ui.stencil.css";
 import "../../joint/joint.ui.selectionView";
@@ -10,10 +12,13 @@ import "../../joint/joint.ui.halo";
 import "../../joint/br-scroller";
 import "../../joint/joint.dia.command";
 import shapes from "../../joint/shapes";
-import jointCss from "jointjs/dist/joint.min.css";
+joint.shapes.erd = shapes;
+
 import angular from "angular";
 import template from "./conceptual.html";
+
 import modelDuplicatorComponent from "../components/duplicateModelModal";
+
 import Factory from "./factory";
 import Validator from "./validator";
 import Linker from "./linker";
@@ -24,24 +29,7 @@ import preventExitServiceModule from "../service/preventExitService";
 import statusBar from "../components/statusBar";
 import bugReportButton from "../components/bugReportButton";
 
-joint.shapes.erd = shapes;
-
-/*
- * This line prevent a sideEffect issue in jointjs library that make webpack ignore joint css imports
- * See more: https://github.com/webpack/webpack/issues/8814
- */
-console.log(jointCss);
-
-const controller = function (
-	ModelAPI,
-	$stateParams,
-	$rootScope,
-	$timeout,
-	$uibModal,
-	$state,
-	$transitions,
-	preventExitService
-) {
+const controller = function (ModelAPI, $stateParams, $rootScope, $timeout, $uibModal, $state, $transitions,preventExitService) {
 	const ctrl = this;
 	ctrl.modelState = {
 		isDirty: false,
@@ -49,16 +37,16 @@ const controller = function (
 	};
 	ctrl.feedback = {
 		message: "",
-		showing: false,
-	};
+		showing: false
+	}
 	ctrl.loading = true;
 	ctrl.model = {
-		id: "",
-		name: "",
-		type: "conceptual",
-		model: "",
-		user: $rootScope.loggeduser,
-	};
+		id: '',
+		name: '',
+		type: 'conceptual',
+		model: '',
+		user: $rootScope.loggeduser
+	}
 	ctrl.selectedElement = {};
 	ctrl.selectedHalo = {};
 	const configs = {
@@ -77,37 +65,37 @@ const controller = function (
 		$timeout(() => {
 			ctrl.loading = show;
 		});
-	};
+	}
 
 	ctrl.showFeedback = (show, newMessage) => {
 		$timeout(() => {
 			ctrl.feedback.showing = show;
 			ctrl.feedback.message = newMessage;
 		});
-	};
+	}
 
 	ctrl.saveModel = () => {
+		ctrl.modelState.updatedAt = new Date()
 		setIsDirty(false);
-		ctrl.modelState.updatedAt = new Date();
 		ctrl.setLoading(true);
 		ctrl.model.model = JSON.stringify(configs.graph);
 		ModelAPI.updateModel(ctrl.model).then(function (res) {
 			ctrl.showFeedback(true, "Salvo com sucesso!");
 			ctrl.setLoading(false);
 		});
-	};
+	}
 
 	ctrl.print = () => {
 		window.print();
-	};
+	}
 
 	ctrl.undoModel = () => {
 		configs.commandManager.undo();
-	};
+	}
 
 	ctrl.redoModel = () => {
 		configs.commandManager.redo();
-	};
+	}
 
 	ctrl.zoomIn = () => {
 		configs.paperScroller.zoom(0.1, { max: 2 });
@@ -124,13 +112,12 @@ const controller = function (
 	ctrl.duplicateModel = (model) => {
 		const modalInstance = $uibModal.open({
 			animation: true,
-			template:
-				'<duplicate-model-modal suggested-name="$ctrl.suggestedName" close="$close(result)" dismiss="$dismiss(reason)"></duplicate-model-modal>',
+			template: '<duplicate-model-modal suggested-name="$ctrl.suggestedName" close="$close(result)" dismiss="$dismiss(reason)"></duplicate-model-modal>',
 			controller: function () {
 				const $ctrl = this;
 				$ctrl.suggestedName = `${model.name} (cópia)`;
 			},
-			controllerAs: "$ctrl",
+			controllerAs: '$ctrl',
 		});
 		modalInstance.result.then((newName) => {
 			ctrl.setLoading(true);
@@ -142,7 +129,7 @@ const controller = function (
 				user: model.who,
 			};
 			ModelAPI.saveModel(duplicatedModel).then((newModel) => {
-				window.open($state.href("conceptual", { modelid: newModel._id }));
+				window.open($state.href('conceptual', { 'modelid': newModel._id }));
 				ctrl.showFeedback(true, "Duplicado com sucesso!");
 				ctrl.setLoading(false);
 			});
@@ -151,23 +138,16 @@ const controller = function (
 
 	ctrl.convertModel = (conceptualModel) => {
 		const model = {
-			name: conceptualModel.name + "_convertido",
-			user: $rootScope.loggeduser,
-			type: "logic",
-			model: '{"cells":[]}',
+			"name": conceptualModel.name + "_convertido",
+			"user": $rootScope.loggeduser,
+			"type": "logic",
+			"model": '{"cells":[]}'
 		};
-		ModelAPI.saveModel(model).then((newModel) => {
-			window.open(
-				$state.href("logic", {
-					references: {
-						modelid: newModel._id,
-						conversionId: conceptualModel._id,
-					},
-				}),
-				"_blank"
-			);
-		});
-	};
+		ModelAPI.saveModel(model)
+			.then((newModel) => {
+				window.open($state.href('logic', { references: { 'modelid': newModel._id, 'conversionId': conceptualModel._id } }), '_blank');
+			});
+	}
 
 	ctrl.unselectAll = () => {
 		ctrl.showFeedback(false, "");
@@ -189,156 +169,140 @@ const controller = function (
 					element: cellView
 				}
 			});
-			return;
+			return
 		}
 
 		$timeout(() => {
 			ctrl.selectedElement = {
 				value: "",
 				type: "blank",
-				element: null,
-			};
+				element: null
+			}
 		});
-	};
+	}
 
 	ctrl.onUpdate = (event) => {
 		switch (event.type) {
-			case "name":
+			case 'name':
 				$timeout(() => {
-					ctrl.selectedElement.element.model.attributes.attrs.text.text =
-						event.value;
+					ctrl.selectedElement.element.model.attributes.attrs.text.text = event.value;
 					ctrl.selectedElement.element.update();
 				});
 				break;
-			case "extention":
+			case 'extention':
 				$timeout(() => {
 					if (ctrl.selectedElement.element.model.attributes.isExtended) {
-						ctrl.entityExtensor.updateExtension(
-							ctrl.selectedElement.element,
-							event.value
-						);
+						ctrl.entityExtensor.updateExtension(ctrl.selectedElement.element, event.value);
 						ctrl.selectedElement.element.update();
 					} else {
-						ctrl.entityExtensor.createExtension(
-							ctrl.selectedElement.element,
-							event.value
-						);
+						ctrl.entityExtensor.createExtension(ctrl.selectedElement.element, event.value);
 					}
 				});
 				break;
-			case "editExtention":
+			case 'editExtention':
 				$timeout(() => {
-					ctrl.selectedElement.element.model.attributes.attrs.text.text =
-						event.value;
+					ctrl.selectedElement.element.model.attributes.attrs.text.text = event.value;
 					ctrl.selectedElement.element.update();
 				});
 				break;
-			case "addAutoRelationship":
+			case 'addAutoRelationship':
 				$timeout(() => {
 					ctrl.shapeLinker.addAutoRelationship(ctrl.selectedElement);
 				});
 				break;
-			case "link.cardinality":
+			case 'link.cardinality':
 				$timeout(() => {
-					ctrl.selectedElement.element.model.label(0, {
-						position: 0.3,
-						attrs: { text: { text: event.value } },
-					});
+					ctrl.selectedElement.element.model.label(0,
+						{
+							position: 0.3,
+							attrs: { text: { text: event.value } }
+						});
 				});
 				break;
-			case "link.role":
+			case 'link.role':
 				$timeout(() => {
-					ctrl.selectedElement.element.model.label(1, {
-						position: 0.7,
-						attrs: { text: { text: event.value } },
-					});
+					ctrl.selectedElement.element.model.label(1,
+						{
+							position: 0.7,
+							attrs: { text: { text: event.value } }
+						});
 				});
 				break;
-			case "link.weak":
+			case 'link.weak':
 				$timeout(() => {
 					if (event.value) {
 						ctrl.selectedElement.element.model.attributes.attrs = {
-							".connection": { stroke: "black", "stroke-width": 3 },
+							'.connection': { stroke: 'black', 'stroke-width': 3 }
 						};
 					} else {
 						ctrl.selectedElement.element.model.attributes.attrs = {
-							".connection": { stroke: "black", "stroke-width": 1 },
+							'.connection': { stroke: 'black', 'stroke-width': 1 }
 						};
 					}
 					ctrl.selectedElement.element.model.attributes.weak = event.value;
 					ctrl.selectedElement.element.update();
 				});
 				break;
-			case "attribute.cardinality":
+			case 'attribute.cardinality':
 				$timeout(() => {
 					const newCardinality = event.value;
 					let currentText = ctrl.selectedElement.value.name;
 
-					if (newCardinality != "(1, 1)") {
+					if(newCardinality != '(1, 1)'){
 						currentText = currentText + " " + newCardinality;
 					}
 
-					ctrl.selectedElement.element.model.attributes.attrs.text.text =
-						currentText;
-					ctrl.selectedElement.element.model.attributes.cardinality =
-						newCardinality;
+					ctrl.selectedElement.element.model.attributes.attrs.text.text = currentText;
+					ctrl.selectedElement.element.model.attributes.cardinality = newCardinality;
 					ctrl.selectedElement.element.update();
 				});
 				break;
-			case "attribute.name":
+			case 'attribute.name':
 				$timeout(() => {
 					let newName = event.value;
 					const currentCardinality = ctrl.selectedElement.value.cardinality;
 
-					if (currentCardinality != "(1, 1)") {
+					if(currentCardinality != '(1, 1)'){
 						newName = newName + " " + currentCardinality;
 					}
 
-					ctrl.selectedElement.element.model.attributes.attrs.text.text =
-						newName;
+					ctrl.selectedElement.element.model.attributes.attrs.text.text = newName;
 					ctrl.selectedElement.element.update();
 				});
 				break;
-			case "attribute.composed":
+			case 'attribute.composed':
 				$timeout(() => {
 					const newValue = event.value;
 					const root = ctrl.selectedElement.element.model;
-					if (newValue) {
-						const rootX = root.attributes.position.x;
-						const rootY = root.attributes.position.y;
+					if(newValue) {
+							const rootX = root.attributes.position.x;
+							const rootY = root.attributes.position.y;
 
-						const attr1 = ctrl.shapeFactory.createAttribute({
-							position: { x: rootX + 50, y: rootY + 20 },
-						});
-						attr1.attributes.attrs.text.text = "attr1";
+							const attr1 = ctrl.shapeFactory.createAttribute({ "position": { x: rootX + 50, y: rootY + 20 }});
+							attr1.attributes.attrs.text.text = "attr1";
 
-						configs.graph.addCell(attr1);
-						ctrl.shapeLinker.createLink(root, attr1, configs.graph);
+							configs.graph.addCell(attr1);
+							ctrl.shapeLinker.createLink(root, attr1, configs.graph);
 
-						const attr2 = ctrl.shapeFactory.createAttribute({
-							position: { x: rootX + 50, y: rootY - 20 },
-						});
-						attr2.attributes.attrs.text.text = "attr2";
+							const attr2 = ctrl.shapeFactory.createAttribute({ "position": { x: rootX + 50, y: rootY - 20 }});
+							attr2.attributes.attrs.text.text = "attr2";
 
-						configs.graph.addCell(attr2);
-						ctrl.shapeLinker.createLink(root, attr2, configs.graph);
+							configs.graph.addCell(attr2);
+							ctrl.shapeLinker.createLink(root, attr2, configs.graph);
 					} else {
-						configs.graph
-							.getNeighbors(root)
-							.filter((neighbor) => ctrl.shapeValidator.isAttribute(neighbor))
-							.forEach((neighbor) => neighbor.remove());
+						configs.graph.getNeighbors(root)
+							.filter(neighbor => ctrl.shapeValidator.isAttribute(neighbor))
+							.forEach(neighbor => neighbor.remove());
 					}
 				});
 				break;
-			case "relationship.associative":
+			case 'relationship.associative':
 				$timeout(() => {
 					const relationship = ctrl.selectedElement.element.model;
-					if (relationship.attributes.parent == null) {
+					if(relationship.attributes.parent == null){
 						const posX = relationship.attributes.position.x;
 						const posY = relationship.attributes.position.y;
-						const block = ctrl.shapeFactory.createBlockAssociative({
-							position: { x: posX - 6, y: posY - 2 },
-						});
+						const block = ctrl.shapeFactory.createBlockAssociative({ "position": { x: posX - 6, y: posY - 2 }});
 
 						configs.graph.addCell(block);
 
@@ -348,51 +312,41 @@ const controller = function (
 				});
 				break;
 		}
-	};
+	}
 
 	ctrl.makeAssociative = (model) => {
 		const posX = model.attributes.position.x;
 		const posY = model.attributes.position.y;
-		const block = ctrl.shapeFactory.createBlockAssociative({
-			position: { x: posX, y: posY },
-		});
-		const auto = ctrl.shapeFactory.createRelationship({
-			position: { x: posX + 6, y: posY + 2 },
-		});
+		const block = ctrl.shapeFactory.createBlockAssociative({ "position": { x: posX, y: posY }});
+		const auto = ctrl.shapeFactory.createRelationship({ position: { x: posX + 6, y: posY + 2 }});
 
 		block.embed(auto);
 		configs.graph.addCells([block, auto]);
 
 		$timeout(() => {
 			model.remove();
-		});
-	};
+		})
+	}
 
 	ctrl.makeComposedAttribute = (model) => {
 		$timeout(() => {
 			const posX = model.attributes.position.x;
 			const posY = model.attributes.position.y;
-			const base = ctrl.shapeFactory.createAttribute({
-				position: { x: posX, y: posY },
-			});
+			const base = ctrl.shapeFactory.createAttribute({ "position": { x: posX, y: posY }});
 			base.attributes.composed = true;
 
-			const attr1 = ctrl.shapeFactory.createAttribute({
-				position: { x: posX + 50, y: posY + 20 },
-			});
+			const attr1 = ctrl.shapeFactory.createAttribute({ "position": { x: posX + 50, y: posY + 20 }});
 			attr1.attributes.attrs.text.text = "attr1";
 
-			const attr2 = ctrl.shapeFactory.createAttribute({
-				position: { x: posX + 50, y: posY - 20 },
-			});
+			const attr2 = ctrl.shapeFactory.createAttribute({ "position": { x: posX + 50, y: posY - 20 }});
 			attr2.attributes.attrs.text.text = "attr2";
 
 			configs.graph.addCells([base, attr1, attr2]);
 			ctrl.shapeLinker.createLink(base, attr1, configs.graph);
 			ctrl.shapeLinker.createLink(base, attr2, configs.graph);
 			model.remove();
-		}, 100);
-	};
+		}, 100)
+	}
 
 	const registerPaperEvents = (paper) => {
 		paper.on('blank:pointerdown', (evt) => {
@@ -404,11 +358,11 @@ const controller = function (
 			}
 		});
 
-		paper.on("link:options", (cellView) => {
+		paper.on('link:options', (cellView) => {
 			ctrl.onSelectElement(cellView);
 		});
 
-		paper.on("element:pointerup", (cellView, evt, x, y) => {
+		paper.on('element:pointerup', (cellView, evt, x, y) => {
 			ctrl.onSelectElement(cellView);
 			// if(x != null && y != null){
 			// 	$scope.conectElements(cellView, x, y)
@@ -417,7 +371,7 @@ const controller = function (
 
 			const halo = new joint.ui.Halo({
 				cellView: cellView,
-				boxContent: false,
+				boxContent: false
 			});
 
 			configs.selectedHalo = halo;
@@ -425,16 +379,13 @@ const controller = function (
 				ctrl.shapeLinker.onLink(link);
 			});
 
-			if (
-				ctrl.shapeValidator.isAttribute(cellView.model) ||
-				ctrl.shapeValidator.isExtension(cellView.model)
-			) {
-				halo.removeHandle("resize");
+			if (ctrl.shapeValidator.isAttribute(cellView.model) || ctrl.shapeValidator.isExtension(cellView.model)) {
+				halo.removeHandle('resize');
 			}
 
-			halo.removeHandle("clone");
-			halo.removeHandle("fork");
-			halo.removeHandle("rotate");
+			halo.removeHandle('clone');
+			halo.removeHandle('fork');
+			halo.removeHandle('rotate');
 			halo.render();
 		});
 
@@ -460,6 +411,7 @@ const controller = function (
 	}
 
 	const registerGraphEvents = (graph) => {
+
 		graph.on("change", () => {
 			setIsDirty(true);
 		});
@@ -474,26 +426,24 @@ const controller = function (
 			const parent = configs.graph.getCell(parentId);
 			const parentBbox = parent.getBBox();
 			const cellBbox = cell.getBBox();
-			if (
-				parentBbox.containsPoint(cellBbox.origin()) &&
+			if (parentBbox.containsPoint(cellBbox.origin()) &&
 				parentBbox.containsPoint(cellBbox.topRight()) &&
 				parentBbox.containsPoint(cellBbox.corner()) &&
-				parentBbox.containsPoint(cellBbox.bottomLeft())
-			) {
-				return;
-			}
-			cell.set("position", cell.previous("position"));
+				parentBbox.containsPoint(cellBbox.bottomLeft())) {
+					return;
+				}
+			cell.set('position', cell.previous('position'));
 		});
 
 		graph.on('add', (model) => {
 			setIsDirty(true);
 			if (model instanceof joint.dia.Link) return;
 
-			if (ctrl.shapeValidator.isAssociative(model)) {
+			if(ctrl.shapeValidator.isAssociative(model)) {
 				ctrl.makeAssociative(model);
 			}
 
-			if (ctrl.shapeValidator.isComposedAttribute(model)) {
+			if(ctrl.shapeValidator.isComposedAttribute(model)) {
 				ctrl.makeComposedAttribute(model);
 			}
 
@@ -505,15 +455,15 @@ const controller = function (
 			// 		}
 			// 	}
 		});
-	};
+
+	}
 
 	const buildWorkspace = () => {
 		configs.graph = new joint.dia.Graph({}, { cellNamespace: joint.shapes });
+
 		registerGraphEvents(configs.graph);
 
-		configs.commandManager = new joint.dia.CommandManager({
-			graph: configs.graph,
-		});
+		configs.commandManager = new joint.dia.CommandManager({ graph: configs.graph })
 
 		const content = $("#content");
 
@@ -524,18 +474,14 @@ const controller = function (
 			drawGrid: true,
 			model: configs.graph,
 			linkConnectionPoint: joint.util.shapePerimeterConnectionPoint,
-			cellViewNamespace: joint.shapes,
+			cellViewNamespace: joint.shapes
 		});
 
 		configs.keyboardController = new KeyboardController(configs.paper.$document);
 
 		registerPaperEvents(configs.paper);
 
-		configs.selectionView = new joint.ui.SelectionView({
-			paper: configs.paper,
-			graph: configs.graph,
-			model: new Backbone.Collection(),
-		});
+		configs.selectionView = new joint.ui.SelectionView({ paper: configs.paper, graph: configs.graph, model: new Backbone.Collection });
 
 		configs.paperScroller = new joint.ui.PaperScroller({
 			paper: configs.paper,
@@ -558,9 +504,7 @@ const controller = function (
 			ctrl.shapeFactory.createAssociative({ position: { x: 15, y: 185 } }),
 			ctrl.shapeFactory.createAttribute({ position: { x: 65, y: 265 } }),
 			ctrl.shapeFactory.createKey({ position: { x: 65, y: 305 } }),
-			ctrl.shapeFactory.createComposedAttribute({
-				position: { x: 30, y: 345 },
-			}),
+			ctrl.shapeFactory.createComposedAttribute({ position: { x: 30, y: 345 } }),
 		]);
 
 		registerShortcuts();
@@ -609,8 +553,7 @@ const controller = function (
 };
 
 export default angular
-	.module("app.workspace.conceptual", [modelDuplicatorComponent, preventExitServiceModule, statusBar,
-		bugReportButton])
+	.module("app.workspace.conceptual", [modelDuplicatorComponent, preventExitServiceModule, statusBar, bugReportButton])
 	.component("editorConceptual", {
 		template,
 		controller,
