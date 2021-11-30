e6eb4621a9f5cd808b53f2ddef7cf288e8c186d4
Walmyr <wlsf82@gmail.com>
2021-11-30 02:10:05 +0100

    Refactor tests and add new ones
    Note: one of the new tests is failing due to a translation issue.
    The one that duplicated a model.
    
    This commit also fixes linting issues on test files.
    
    By the way, we should talk about the eslint config.
    To me, it seems odd.

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,79 +1,61 @@
-describe('Login', () => {
-  const userLoginData = {
-    user: Cypress.env('user'),
-    password: Cypress.env('password'),
-    useCachedSession: {
-      cacheSession: false
-    }
-  }
+describe("Login", () => {
+	const userLoginData = {
+		user: Cypress.env("user"),
+		password: Cypress.env("password"),
+		useCachedSession: {
+			cacheSession: false,
+		},
+	};
 
-  const { user, password, useCachedSession } = userLoginData
+	const { user, password, useCachedSession } = userLoginData;
 
-  it('logs in successfully', () => {
-    cy.intercept('GET', '/models?userId=*').as('getUserModels')
+	it("logs in successfully", () => {
+		cy.intercept("GET", "/models?userId=*").as("getUserModels");
 
-    cy.login(user, password, useCachedSession)
+		cy.login(user, password, useCachedSession);
 
-    cy.wait('@getUserModels')
-    cy.url()
-      .should('be.equal', `${Cypress.config('baseUrl')}/#!/main`)
-    cy.contains('h2', 'Models')
-      .should('be.visible')
-  })
+		cy.wait("@getUserModels");
+		cy.url().should("be.equal", `${Cypress.config("baseUrl")}/#!/main`);
+		cy.contains("h2", "Models").should("be.visible");
+	});
 
-  context('Form validations', () => {
-    it('alerts on invalid user email', () => {
-      cy.login('invalid#user.com', password, useCachedSession)
+	context("Form validations", () => {
+		it("alerts on invalid user email", () => {
+			cy.login("invalid#user.com", password, useCachedSession);
 
-      cy.contains('.alert-danger', 'Fill the fields in red')
-        .should('be.visible')
-      cy.get('#userEmail')
-        .should('have.class', 'error')
-      cy.get('.error')
-          .should('have.length', 1)
-          .and('be.visible')
-    })
+			cy.contains(".alert-danger", "Fill the fields in red").should(
+				"be.visible"
+			);
+			cy.get("#userEmail").should("have.class", "error");
+			cy.get(".error").should("have.length", 1).and("be.visible");
+		});
 
-    it('alerts when clicking ENTER without filling the form', () => {
-      cy.visit('/')
-      cy.contains('button', 'Login')
-        .click()
+		it("alerts when clicking ENTER without filling the form", () => {
+			cy.visit("/");
+			cy.contains("button", "Login").click();
 
-      cy.contains('.alert-danger', 'Fill the fields in red')
-        .should('be.visible')
-      cy.get('#userEmail')
-        .should('have.class', 'error')
-      cy.get('#userPassword')
-        .should('have.class', 'error')
-      cy.get('.error')
-        .should('have.length', 2)
-        .and('be.visible')
-    })
+			cy.contains(".alert-danger", "Fill the fields in red").should(
+				"be.visible"
+			);
+			cy.get("#userEmail").should("have.class", "error");
+			cy.get("#userPassword").should("have.class", "error");
+			cy.get(".error").should("have.length", 2).and("be.visible");
+		});
 
-    it('alerts on invalid user', () => {
-      cy.login('invalid@user.com', password, useCachedSession)
+		it("alerts on invalid user", () => {
+			cy.login("invalid@user.com", password, useCachedSession);
 
-      cy.contains('.alert-danger', 'Incorrect login or password')
-        .should('be.visible')
-    })
+			cy.contains(".alert-danger", "Incorrect login or password").should(
+				"be.visible"
+			);
+		});
 
-    it('alerts on invalid password', () => {
-      cy.login(Cypress.env('user'), 'invalid-pwd', useCachedSession)
+		it("alerts on invalid password", () => {
+			cy.login(user, "invalid-pwd", useCachedSession);
 
-      cy.contains('.alert-danger', 'Incorrect login or password')
-        .should('be.visible')
-    })
-
-    it('change language', () => {
-      cy.visit('/')
-
-      cy.get('[alt="Brazil flag"]').click()
-      cy.contains('button', 'Entrar')
-        .should('be.visible')
-
-      cy.get('[alt="United States flag"]').click()
-      cy.contains('button', 'Login')
-        .should('be.visible') 
-    })
-  })
-})
+			cy.contains(".alert-danger", "Incorrect login or password").should(
+				"be.visible"
+			);
+		});
+	});
+});
