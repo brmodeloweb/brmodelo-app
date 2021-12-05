b4359ea15e88a8e9fd2b3b361e00aec36a268108
Márcio <marcio.tbms@gmail.com>
2021-12-05 16:19:26 +0100

    refactor: removes unused component pages

--- app/react/pages/Login/index.ts
+++
@@ -1,8 +0,0 @@
-import angular from "angular";
-import { react2angular } from "react2angular";
-import Login from "./Login";
-
-export { Login };
-export default angular
-	.module("app.react.login", [])
-	.component("reactLoginPage", react2angular(Login)).name;
