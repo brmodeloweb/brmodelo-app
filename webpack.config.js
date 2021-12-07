d4bc313e417631712ec6fc44a9d50c3f10d1bc06
Márcio <marcio.tbms@gmail.com>
2021-12-07 22:56:58 +0100

    fix: webpack font loader

--- webpack.config.js
+++ webpack.config.js
@@ -78,15 +78,7 @@ module.exports = {
 			},
 			{
 				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[name].[ext]",
-							outputPath: "fonts/",
-						},
-					},
-				],
+				type: "asset/resource",
 			},
 		],
 	},
