589d135169885f11d39ed09fff635b1ab70adef5
Márcio <marcio.tbms@gmail.com>
2021-12-07 22:43:03 +0100

    fix: removes unused import

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 	mode: process.env.NODE_ENV,
