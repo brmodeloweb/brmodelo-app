9f1dae0bdcc2bdeb267822efeed5b50692ba62fd
Márcio <marcio.tbms@gmail.com>
2021-12-14 20:28:18 +0100

    refactor: Renames wrapper component from Providers to BasePage

--- app/react/containers/BasePage/BasePage.tsx
+++ app/react/containers/BasePage/BasePage.tsx
@@ -1,20 +1,22 @@
-import i18n from "i18n";
 import PropTypes from "prop-types";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "@styles/GlobalStyles";
+import i18n from "../../i18n";
 import theme from "../../theme";
 
-const Providers: React.FC = ({ children }) => {
+const BasePage: React.FC = ({ children }) => {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalStyles />
 			<I18nextProvider i18n={i18n}>{children}</I18nextProvider>
 		</ThemeProvider>
 	);
 };
 
-Providers.propTypes = {
+BasePage.propTypes = {
 	children: PropTypes.element.isRequired,
 };
 
-export default Providers;
+export default BasePage;
---
+++ app/react/containers/BasePage/index.ts
@@ -0,0 +1,3 @@
+import BasePage from "./BasePage";
+
+export default BasePage;
