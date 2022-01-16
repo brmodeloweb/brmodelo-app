f0df83673bd5ce13c99abeaecf473fcad21891e9
Arthur Mota <arthurmotaa9@gmail.com>
2022-01-16 17:37:37 -0300

    Creating sql comparasion dropdown component

--- app/angular/logic/checkConstraint.js
+++ app/angular/logic/checkConstraint.js
@@ -12,26 +12,6 @@ const Controller = function () {
 		$ctrl.isManual = !$ctrl.isManual;
 	}
 
-	$ctrl.checkConstraintOptions = {
-		NUMBER: [
-			{ name: 'Equal to', type: 'EQUAL_TO' },
-			{ name: 'Not equal to', type: 'NOT_EQUAL_TO' },
-			{ name: 'Greater than', type: 'GREATER_THAN' },
-			{ name: 'Less than', type: 'LESS_THAN' },
-			{ name: 'Between', type: 'BETWEEN' },
-			{ name: 'Greater than or equal to', type: 'GREATER_THAN_OR_EQUAL_TO' },
-			{ name: 'Less than or equal to', type: 'LESS_THAN_OR_EQUAL_TO' },
-		],
-		STRING: [
-			{ name: 'Is', type: 'IS' },
-			{ name: 'Is not', type: 'IS_NOT' },
-			{ name: 'Contains', type: 'CONTAINS' },
-			{ name: 'Does not contain', type: 'DOES_NOT_CONTAIN' },
-			{ name: 'Starts with', type: 'STARTS_WITH' },
-			{ name: 'Ends with', type: 'ENDS_WITH' },
-		]
-	};
-
 	$ctrl.selectCheckConstraint = (selected) => {
 		$ctrl.column.checkConstraint = {
 			type: selected.type,
