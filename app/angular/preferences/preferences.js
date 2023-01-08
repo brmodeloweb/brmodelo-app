9a00aa7f77cca0e6f7fd5ef601a0d35c90b72606
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2023-01-08 19:30:07 +0100

    WIP: Add Preferences page

---
+++ app/angular/preferences/preferences.js
@@ -0,0 +1,12 @@
+import angular from "angular";
+import template from "./preferences.html";
+
+const preferencesController = {
+	// Soon
+}
+
+export default angular
+	.module("app.preferences").component("preferences", {
+		template,
+		controller: preferencesController,
+	}).name;
