12fb85f94c45a97e62fef308c047a8905cb4626a
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2023-01-08 19:44:44 +0100

    Preferences page
    - Enable translations,
    - Add missing array when exporting app.preferences

--- app/angular/preferences/preferences.js
+++ app/angular/preferences/preferences.js
@@ -1,12 +1,13 @@
 import angular from "angular";
 import template from "./preferences.html";
 
-const preferencesController = {
-	// Soon
+const preferencesController = function($translate){
+
 }
 
 export default angular
-	.module("app.preferences").component("preferences", {
+	.module("app.preferences", [])
+	.component("preferences", {
 		template,
 		controller: preferencesController,
 	}).name;
