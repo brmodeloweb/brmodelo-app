cece6008002eafd12dae74a03e9acc28c0e42c6e
Walmyr <wlsf82@gmail.com>
2023-11-13 00:11:14 +0100

    Define api url as a cypress env

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  apiUrl: 'http://localhost:3000',
+  env: {
+    apiUrl: 'http://localhost:3000',
+  },
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:9000',
