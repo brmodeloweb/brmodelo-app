7894bc9d76616835554e61886586621a73a3a33e
Milton Bittencourt <miltonbst@gmail.com>
2024-04-24 21:58:28 -0300

    Create import modal basic function

--- app/angular/components/importModelModal.js
+++ app/angular/components/importModelModal.js
@@ -3,13 +3,27 @@ import template from "./ImportModelModal.html";
 
 const app = angular.module("app.ImportModelModal", []);
 
-const Controller = function () {
+const Controller = function (ModelAPI, AuthService) {
 	const $ctrl = this;
 	$ctrl.submitted = false;
+
+	$ctrl.$onInit = () => {
+		$ctrl.url = "teste";
+	};
+
 
 	$ctrl.cancel = () => {
 		$ctrl.dismiss({
 			reason: "cancel",
+		});
+	};
+
+	$ctrl.save = (url) => {
+		const parts = url.split("/");
+		const shareId = parts[parts.length-1];
+		console.log(AuthService);
+		ModelAPI.importModel(shareId, AuthService.loggeduser).then(response => {
+			$ctrl.close({result: response.data});
 		});
 	};
 };
