ff3b8ceb43a7b40c6426dc08aa46c62dd0c2493a
Milton Bittencourt <miltonbst@gmail.com>
2025-04-21 16:40:24 -0300

    nosql sidebar injection

--- app/angular/nosql/sidebarControl.js
+++ app/angular/nosql/sidebarControl.js
@@ -4,13 +4,7 @@ import template from "./sidebarControl.html";
 const configurator = () => {
 
 	const configuration = {
-		"emptyState": false,
-		"entity": false,
-		"relationship": false,
-		"extension": false,
-		"attribute": false,
-		"connection": false,
-		"link": false
+		"emptyState": false
 	}
 
 	const emptyState = () => {
@@ -19,28 +13,7 @@ const configurator = () => {
 	}
 
 	const select = (element) => {
-		switch (element.type) {
-			case "Entity":
-				configuration.entity = true;
-				return configuration;
-			case "Attribute":
-				configuration.attribute = true;
-				return configuration;
-			case "Key":
-				configuration.key = true;
-				return configuration;
-			case "Relationship":
-				configuration.relationship = true;
-				return configuration;
-			case "Inheritance":
-				configuration.extension = true;
-				return configuration;
-			case "Link":
-				configuration.link = true;
-				return configuration;
-			default:
-				break;
-		}
+		switch (element.type) {}
 
 		return emptyState();
 	}
@@ -78,106 +51,7 @@ const controller = function($rootScope, $timeout) {
 		}
 	}
 
-	$ctrl.extendEntity = (selected) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "extention",
-				"value": selected.type
-			}
-		});
-	}
-
-	$ctrl.editExtention = (selected) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "editExtention",
-				"value": selected
-			}
-		});
-	}
-
-	$ctrl.updateCardinality = (selected) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "link.cardinality",
-				"value": selected.type
-			}
-		});
-	}
-
-	$ctrl.updateLinkRole = (selected) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "link.role",
-				"value": selected
-			}
-		});
-	}
-
-	$ctrl.updateLinkWeak = (selected) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "link.weak",
-				"value": selected
-			}
-		});
-	}
-
-	$ctrl.updateAttributeCardinality = (selected) => {
-		$ctrl.selectedElement.value.cardinality = selected.type;
-		$ctrl.onUpdate({
-			"event": {
-				"type": "attribute.cardinality",
-				"value": selected.type
-			}
-		});
-	}
-
-	$ctrl.updateAttributeName = (newName) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "attribute.name",
-				"value": newName
-			}
-		});
-	}
-
-	$ctrl.swapComposed = (value) => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "attribute.composed",
-				"value": value
-			}
-		});
-	}
-
-	$ctrl.transformAssociative = () => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "relationship.associative",
-			}
-		});
-	}
-
 	const customSelector = (selected) => {
-		const currentType = selected.currentValue.type;
-		if (currentType === "Link") {
-			const attributes = selected.currentValue.element.model.attributes;
-			selected.currentValue.value = {
-				"weak": attributes.weak,
-				"role": attributes.role,
-				"cardinality": attributes.labels[0].attrs.text.text
-			}
-		}
-
-		if (currentType === "Attribute") {
-			const attributes = selected.currentValue.element.model.attributes;
-			selected.currentValue.value = {
-				"name": attributes.attrs.text.text.replace(/ *\([^)]*\) */g, ""),
-				"cardinality": attributes.cardinality,
-				"composed": attributes.composed
-			}
-		}
 		return selected.currentValue;
 	}
 
@@ -192,19 +66,11 @@ const controller = function($rootScope, $timeout) {
 		$ctrl.visible = !$ctrl.visible;
 	}
 
-	$ctrl.addAutoRelationship = () => {
-		$ctrl.onUpdate({
-			"event": {
-				"type": "addAutoRelationship"
-			}
-		});
-	}
-
 }
 
 export default angular
-	.module("app.workspace.conceptual.sidebar", [])
-	.component("sidebarControlConceptual", {
+	.module("app.workspace.nosql.sidebar", [])
+	.component("sidebarControlNosql", {
 		template,
 		controller,
 		bindings: {
