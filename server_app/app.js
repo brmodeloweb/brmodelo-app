16bc90472a232ee2a76f1cbb25976a647a67e402
Milton Bittencourt <miltonbst@gmail.com>
2025-11-03 21:28:51 -0300

    Remove morgan dependency

--- server_app/app.js
+++ server_app/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const errorhandler = require("errorhandler");
-const morgan = require("morgan");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -20,7 +19,6 @@ if (app.get("env") === "production") {
 const viewsPath = path.join(__dirname, "../views");
 app.set("views", viewsPath);
 
-app.use(morgan("dev"));
 app.use(bodyParser.json({limit: '50mb'})); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'})); // support encoded bodies
 
